9d788409d951849611430f4f051b91610a3b9990
Eric Krukowski <erickruk@amazon.com>
2023-03-19 20:33:25 -0400

    Form created, duplicates items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById(`create-task-form`).addEventListener('submit', (e)=>{
+    e.preventDefault();
+    addItem(e.target['new-task-description'].value); 
+  });
+
 });
+function addItem(newItem){
+  const newLi = document.createElement('li');
+  newLi.textContent = `${newItem}  `;
+  const newBtn = document.createElement('button');
+  newBtn.textContent = "x";
+  newBtn.addEventListener('click', handleDelete);
+  newLi.appendChild(newBtn);
+  document.getElementById('tasks').appendChild(newLi);
+}
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
